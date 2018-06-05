5fb3d3024fb19942a39738324afb98399079560d
Gergo Angyal <angyalg@gmail.com>
2018-06-05 23:16:22 +0200

    First commit

---
+++ SampleApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleApplication
+//
+//  Created by Gergő Angyal on 2018. 06. 05..
+//  Copyright © 2018. GA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
