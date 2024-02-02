99de7471a161dbdfccd689faa0be53b11c3b4891
Gergo Angyal <angyalg@gmail.com>
2024-02-02 22:18:07 +0100

    One big commit message this is

--- SampleApplication/ViewController.swift
+++ SampleApplication/ViewController.swift
@@ -13,6 +13,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        //first
+
+        // branch first
+
+        // branch second
+
+        // third
     }
 
     override func didReceiveMemoryWarning() {
